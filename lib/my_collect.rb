4eebf5977a8d98e40e0598af066118162dc6bb8c
Sam Ashtar <samashtarm@gmail.com>
2018-08-21 20:16:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect (collection)
   newcollection = []
   counter = 0 
   while collection > counter 
-  newcollection << yield collection[counter]
+  newcollection << yield collection[1]
   counter +=1
 end 
 newcollection
